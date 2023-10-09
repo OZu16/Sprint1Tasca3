574b7f8aa5fff2188ea67b7cb4c1047ce4a14ac1
Gerard <Gerard@TORRE>
2023-10-10 00:15:48 +0200

    nivell 1 exercici 3 acabat

--- Collections/src/n1exercici3/CountriesGame.java
+++ Collections/src/n1exercici3/CountriesGame.java
@@ -18,59 +18,72 @@ public class CountriesGame {
     public static void main(String[] args) {
     	
         Map<String, String> countries = new HashMap<>();
-        Map<String, Integer> rating = new HashMap<>();
-        String rutaCountries = "C:\\Users\\Gerard\\Desktop\\countries\\countries.txt";
-        String rutaClassificacio = "C:\\Users\\Gerard\\Desktop\\countries\\classificacio.txt";
         String userName = "";
-        String userCapital = "";
-        int numRandom = 0;
         int points = 0;
-        
-        try {
-            Scanner scanner = new Scanner(new File(rutaCountries));
-
-            while (scanner.hasNextLine()) {
-                String[] linea = scanner.nextLine().split(" ");
-                countries.put(linea [0], linea[1]);
-            }
-
-            
-        } catch (FileNotFoundException e) {
-            System.out.println("fitxer no trobat");
-        	//e.printStackTrace();
-        }
-
-        /*for (Map.Entry<String, String> entry : lineas.entrySet()) {
-            System.out.println("Clave: " + entry.getKey() + ", Valor: " + entry.getValue());
-        }*/
         
         System.out.println("introdueix nom usuari");
         userName = sc.nextLine();
         
+        textToHashMap(countries);
+        points = gamePoints(countries);
+        textPointsAndUser(points, userName);
         
+    }
+
+    public static void textToHashMap( Map<String, String> countries){
+	    	
+	    	  //Map<String, String> countries = new HashMap<>();
+	          String rutaCountries = "C:\\Users\\Gerard\\Desktop\\countries\\countries.txt";
+	          
+	          try {
+	              Scanner scanner = new Scanner(new File(rutaCountries));
+
+	              while (scanner.hasNextLine()) {
+	                  String[] linea = scanner.nextLine().split(" ");
+	                  countries.put(linea [0], linea[1]);
+	              }
+
+	              
+	          } catch (FileNotFoundException e) {
+	          	e.printStackTrace();
+	          }
+	    }
+	    
+	    
+    public static int gamePoints(Map<String, String> countries) {
+    	
         List<String> keysList = new ArrayList<>(countries.keySet());
         List<String> valueList = new ArrayList<>(countries.values());
+        String userCapital = "";
+    	int numRandom = 0;
+        int points = 0;
 
-	    for(int i = 0; i < 10; i++) {
-	    	numRandom =  (int) (Math.random() * countries.size() + 1);
-	        System.out.println("escriu la capital del seguent pais " + keysList.get(numRandom) + " " + valueList.get(numRandom));
-	        	userCapital = sc.nextLine();
-	        	
-	        if(userCapital.equalsIgnoreCase(valueList.get(numRandom))) {
-	        	points++;
-	        }
-	        rating.put(userName, points);
-        }
-	    	    
+		    for(int i = 0; i < 10; i++) {
+		    	numRandom =  (int) (Math.random() * countries.size() + 1);
+		        System.out.println("escriu la capital del seguent pais " + keysList.get(numRandom));
+		        	userCapital = sc.nextLine();
+		        	
+		        if(userCapital.equalsIgnoreCase(valueList.get(numRandom))) {
+		        	points++;
+		        }
+		    	
+		    }
+	    return points;
+    }
+    
+    public static void textPointsAndUser(int points, String userName) {
+    	
+    	String rutaClassificacio = "C:\\Users\\Gerard\\Desktop\\countries\\classificacio.txt";
+    	
 	    try {
-	    	BufferedWriter writer = new BufferedWriter(new FileWriter(rutaClassificacio));
-	    	writer.write(userName + ": " + points);
-	    	writer.newLine();
+	    	FileWriter writer = new FileWriter(rutaClassificacio);
+	    	writer.write(userName + ": " + points + "\n");
+	    	writer.close();
+	    	
 	    	System.out.println("puntuacio registrada al document classificacio.txt");
 
         } catch (IOException e) {
-            System.out.println("fitxer no trobat");
-            //e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
